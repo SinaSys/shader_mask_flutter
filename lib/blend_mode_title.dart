ed65e23959ac5ad8386a6fb4617604874823c99e
SinaSystem <sinasystem47@gmail.com>
2021-01-25 00:41:07 +0330

    commit DesignEffectApplication

---
+++ lib/blend_mode_title.dart
@@ -0,0 +1,6 @@
+final List<String> blendModeTitle = [
+  "clear","src","dst","srcOver","dstOver","srcIn","dstIn","srcOut","dstOut",
+  "srcATop","dstATop","xor","plus","screen","overlay","darken",
+  "lighten","colorDodge","colorBurn","hardLight","softLight","difference",
+  "exclusion","multiply","hue","saturation","color","luminosity"
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shader_mask_flutter/blend_mode_title.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,21 +16,90 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var blendMode = BlendMode.modulate;
+
+  List<Widget> blendModeButton(){
+    List<Widget> buttonList = [];
+    var raisedBtn;
+    for(int i=0;i<blendModeTitle.length;i++){
+      raisedBtn =  RaisedButton(
+        onPressed: (){},
+        child: Text(blendModeTitle[i],
+        ),);
+      buttonList.add(raisedBtn);
+    }
+    return buttonList;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child:
-          Center(
-            child: ShaderMask(
-              blendMode: BlendMode.srcIn,
+        child: Column(
+          children: [
+            ShaderMask(
+              blendMode: blendMode,
               shaderCallback: (Rect bounds) {
                 return LinearGradient(
-                  colors: [Colors.red, Colors.blue],
-                  tileMode: TileMode.mirror,
-                ).createShader(bounds);
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Colors.red, Colors.black.withOpacity(0.8)])
+                    .createShader(bounds);
               },
-              child: const Text('ShaderMaskTutorial', style: TextStyle(fontSize: 50)),),
-          ),),);}
+              child:Image.asset(
+                'images/wolf.jpg',
+                fit: BoxFit.cover,
+              )
+            ),
+            Expanded(
+              child: GridView.builder(
+                padding:EdgeInsets.all(20),
+                itemCount: blendModeTitle.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 2.0,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 40.0,
+                  crossAxisCount: 3
+                ),
+                  itemBuilder: (context,index){
+                    return RaisedButton(
+                      child: Text(blendModeTitle[index]),
+                        onPressed: (){
+                          BlendMode blendModeEnum = BlendMode.values.firstWhere((e) => e.toString() == 'BlendMode.' + blendModeTitle[index]);
+                          setState(() {
+                            blendMode = blendModeEnum;
+                        // print(blendModeType);
+                       });
+                    });
+
+                  }),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
 }
+
+
+/*Expanded(
+child: SingleChildScrollView(
+child: Wrap(
+spacing: 20,
+runSpacing: 20,
+children:blendModeButton()
+),
+),
+)*/
+
+/*RaisedButton(
+child: Text(""),
+onPressed: (){})*/
