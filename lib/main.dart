296e3afe2503844423dd5d6a6492f5efb5394ed7
SinaSystem <sinasystem47@gmail.com>
2021-01-24 20:25:19 +0330

    add shaderMask Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,26 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Image.asset(
-            'images/wolf.jpg',
-            fit: BoxFit.cover,
+        child: ShaderMask(
+          shaderCallback: (Rect bounds) {
+            return LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.red,
+                  Colors.black.withOpacity(0.8)
+                ]
+            ).createShader(bounds);
+          },
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.asset('images/wolf.jpg',fit: BoxFit.cover,),
           ),
-        ),
-      ),
-    );
-  }
+        ),),);}
 }
