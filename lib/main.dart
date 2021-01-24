cad7ef7fc849cd547720f3320ce18d04dd27828e
SinaSystem <sinasystem47@gmail.com>
2021-01-24 20:31:59 +0330

    commit BlendMode

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,31 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: ShaderMask(
+          blendMode: BlendMode.saturation,
           shaderCallback: (Rect bounds) {
             return LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.red,
-                  Colors.black.withOpacity(0.8)
-                ]
-            ).createShader(bounds);
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.red, Colors.black.withOpacity(0.8)])
+                .createShader(bounds);
           },
           child: Container(
             width: double.infinity,
             height: double.infinity,
-            child: Image.asset('images/wolf.jpg',fit: BoxFit.cover,),
+            child: Image.asset(
+              'images/wolf.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),),);}
+        ),
+      ),
+    );
+  }
 }
